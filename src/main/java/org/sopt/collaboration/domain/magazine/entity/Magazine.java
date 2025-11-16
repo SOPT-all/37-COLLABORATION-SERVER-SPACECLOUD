833d0e3d0c8ad01876b3c5b4d21942b09ce72fdc
kymin <park48664866@gmail.com>
2025-11-16 15:26:51 +0900

    feat: 매거진 엔티티 생성

---
+++ src/main/java/org/sopt/collaboration/domain/magazine/entity/Magazine.java
@@ -0,0 +1,29 @@
+package org.sopt.collaboration.domain.magazine.entity;
+
+import org.sopt.collaboration.domain.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "magazine")
+public class Magazine extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "magazine_image_url", nullable = false)
+	private String magazineImageUrl;
+}
