1bbbcb059d0ed3d0475cd4a47b71dd20cad7582d
kymin <park48664866@gmail.com>
2025-11-17 13:23:17 +0900

    fix: 공간 엔티티 연관관계 추가

--- src/main/java/org/sopt/collaboration/domain/review/entity/Review.java
+++ src/main/java/org/sopt/collaboration/domain/review/entity/Review.java
@@ -1,12 +1,19 @@
 package org.sopt.collaboration.domain.review.entity;
 
 import org.sopt.collaboration.domain.common.BaseEntity;
+import org.sopt.collaboration.domain.place.entity.Place;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -15,7 +22,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "review")
+@Table(name = "review",
+indexes = {
+		@Index(name = "idx_review-place_id", columnList = "place_id")
+})
 public class Review extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,4 +42,8 @@ public class Review extends BaseEntity {
 
 	@Column(name = "review_image_url", nullable = false)
 	private String reviewImageUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "place_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private Place place;
 }
