b56cc7271dee042ae576bb900a4f919571ecfd99
88guri <quffl2002@naver.com>
2025-11-18 02:02:17 +0900

    feat: 리뷰 조회 ResponseDto 구현

---
+++ src/main/java/org/sopt/collaboration/domain/review/dto/response/ReviewResponseDto.java
@@ -0,0 +1,24 @@
+package org.sopt.collaboration.domain.review.dto.response;
+
+import jakarta.persistence.*;
+import org.sopt.collaboration.domain.review.entity.Review;
+
+public record ReviewResponseDto(
+        Long id,
+        int score,
+        String title,
+        String detail,
+        String reviewImageUrl,
+        Long placeId
+) {
+    public static ReviewResponseDto from (Review review) {
+        return new ReviewResponseDto(
+                review.getId(),
+                review.getScore(),
+                review.getTitle(),
+                review.getDetail(),
+                review.getReviewImageUrl(),
+                review.getPlace().getId()
+        );
+    }
+}
