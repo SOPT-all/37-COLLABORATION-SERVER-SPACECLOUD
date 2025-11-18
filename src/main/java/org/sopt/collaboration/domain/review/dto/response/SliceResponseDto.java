a53b935ad8eb2edf88373f9527f4e0b69a126c6c
88guri <quffl2002@naver.com>
2025-11-19 01:10:11 +0900

    refeat: nested record로 변경

--- src/main/java/org/sopt/collaboration/domain/review/dto/response/SliceResponseDto.java
+++ src/main/java/org/sopt/collaboration/domain/review/dto/response/SliceResponseDto.java
@@ -1,5 +1,6 @@
 package org.sopt.collaboration.domain.review.dto.response;
 
+import org.sopt.collaboration.domain.review.entity.Review;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
@@ -13,5 +14,30 @@ public record SliceResponseDto<T>(
                 slice.getContent(),
                 slice.hasNext()
         );
+    }
+
+
+    public record ReviewResponse(
+            Long id,
+            int score,
+            String title,
+            String detail,
+            String reviewImageUrl,
+            Long placeId,
+            List<String> categories
+    ) {
+        public static ReviewResponse from (Review review) {
+            return new ReviewResponse(
+                    review.getId(),
+                    review.getScore(),
+                    review.getTitle(),
+                    review.getDetail(),
+                    review.getReviewImageUrl(),
+                    review.getPlace().getId(),
+                    review.getPlace().getPlaceFilters().stream()
+                            .map(f -> f.getFilter().getFilterCategory().getDescription())
+                            .toList()
+            );
+        }
     }
 }
