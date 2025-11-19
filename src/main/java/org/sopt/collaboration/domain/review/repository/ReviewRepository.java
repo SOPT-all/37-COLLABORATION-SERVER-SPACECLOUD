3a1bf0dc7fec6e4279e5c7456adbc414e7419990
kymin <park48664866@gmail.com>
2025-11-20 00:35:04 +0900

    fix: fetch join적용

--- src/main/java/org/sopt/collaboration/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/sopt/collaboration/domain/review/repository/ReviewRepository.java
@@ -3,12 +3,19 @@ package org.sopt.collaboration.domain.review.repository;
 import org.sopt.collaboration.domain.review.entity.Review;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @EntityGraph(attributePaths = {"place"})
+    @Query("""
+                select r
+                from Review r
+                join fetch r.place p
+                left join fetch p.placeFilters pf
+                left join fetch pf.filter f
+                order by r.createdAt desc
+            """)
     Slice<Review> findByOrderByCreatedAtDesc(Pageable pageable);
 
 }
